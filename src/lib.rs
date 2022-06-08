97589b511bf23a434bbeeef73486177a697f01dc
Piotr Hondra <piotr.hondra@gmail.com>
2022-06-08 22:09:00 +0200

    refactor: Added mod

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-use image::{imageops, RgbImage};
-use numpy::ndarray::{Array3, Axis};
 use numpy::PyArray4;
 use pyo3::prelude::{pymodule, PyModule, PyResult, Python};
 
@@ -8,21 +6,18 @@ fn rust_ext(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     #[pyfn(m)]
     fn process<'py>(_py: Python<'py>, x: &PyArray4<u8>) {
         let mut x = unsafe { x.as_array_mut() };
-        // let mut y = x.axis_iter_mut(Axis(0)).next().unwrap();
-        // &y * 1;
-        // for img in array.axis_iter(Axis(0)) {
-        //     assert!(img.is_standard_layout());
-        //     let (height, width, z) = y.dim();
-        //     let raw = img.to_owned().into_raw_vec();
-        //     let mut img_buff = RgbImage::from_raw(width as u32, height as u32, raw)
-        //         .expect("container should have the right size for the image dimensions");
-        //     imageops::flip_vertical_in_place(&mut img_buff);
-        //     let raw_img_buff = img_buff.into_vec();
-        //     let g = Array3::from_shape_vec((height, width, z), raw_img_buff).unwrap();
-        // }
+        rust_fn::do_sth(&mut x);
+    }
+    Ok(())
+}
+
+mod rust_fn {
+    use image::{imageops, RgbImage};
+    use numpy::ndarray::ArrayViewMut4;
+    use numpy::ndarray::{Array3, Axis};
+
+    pub fn do_sth(x: &mut ArrayViewMut4<'_, u8>) {
         for mut y in x.axis_iter_mut(Axis(0)) {
-            // y.fill(1);
-            // // let mut y = &array.axis_iter_mut(Axis(0)).next().unwrap().to_owned();
             assert!(y.is_standard_layout());
             let (height, width, z) = y.dim();
             let raw = y.to_owned().into_raw_vec();
@@ -33,36 +28,6 @@ fn rust_ext(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
             Array3::from_shape_vec((height, width, z), raw_img)
                 .unwrap()
                 .move_into(&mut y);
-            // Array3::<u8>::zeros((height, width, z)).move_into(&mut y)
-
-            // y = abc.view_mut();
         }
-
-        // y.into_pyarray(py)
     }
-    Ok(())
 }
-
-// mod rust_fn {
-//     use numpy::*;
-
-//     pub fn max_min(x: &ArrayViewD<'_, f64>) -> Array1<f64> {
-//         if x.len() == 0 {
-//             return arr1(&[]); // If the array has no elements, return empty array
-//         }
-//         let max_val = x
-//             .iter()
-//             .map(|a| OrderedFloat(*a))
-//             .max()
-//             .expect("Error calculating max value.")
-//             .0;
-//         let min_val = x
-//             .iter()
-//             .map(|a| OrderedFloat(*a))
-//             .min()
-//             .expect("Error calculating min value.")
-//             .0;
-//         let result_array = arr1(&[max_val, min_val]);
-//         result_array
-//     }
-// }
